d84f4a95c84574aba89d8f955fb0163b1be2ed97
Nicholas Thomson <nithomso@amazon.com>
2022-06-06 19:21:07 +0000

    Initial commit with CertificateAuthority

---
+++ test/e2e/replacement_values.py
@@ -0,0 +1,29 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+#	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+"""Stores the values used by each of the integration tests for replacing the
+EKS-specific test variables.
+"""
+
+from e2e.bootstrap_resources import get_bootstrap_resources
+
+
+REPLACEMENT_VALUES = {
+    "CLUSTER_ROLE": get_bootstrap_resources().ClusterRole.arn,
+    "FARGATE_POD_ROLE": get_bootstrap_resources().FargatePodRole.arn,
+    "NODEGROUP_ROLE": get_bootstrap_resources().NodegroupRole.arn,
+
+    "PUBLIC_SUBNET_1": get_bootstrap_resources().ClusterVPC.public_subnets.subnet_ids[0],
+    "PUBLIC_SUBNET_2": get_bootstrap_resources().ClusterVPC.public_subnets.subnet_ids[1],
+    "PRIVATE_SUBNET_1": get_bootstrap_resources().ClusterVPC.private_subnets.subnet_ids[0],
+    "PRIVATE_SUBNET_2": get_bootstrap_resources().ClusterVPC.private_subnets.subnet_ids[1]
+}
---
+++ test/e2e/service_bootstrap.py
@@ -0,0 +1,47 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+#	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+"""Bootstraps the resources required to run the EKS integration tests.
+"""
+import logging
+
+from acktest.bootstrapping import Resources, BootstrapFailureException
+from acktest.bootstrapping.iam import Role
+from acktest.bootstrapping.vpc import VPC
+from e2e import bootstrap_directory
+from e2e.bootstrap_resources import BootstrapResources
+
+def service_bootstrap() -> Resources:
+    logging.getLogger().setLevel(logging.INFO)
+    
+    resources = BootstrapResources(
+        ClusterRole=Role("cluster-role", "eks.amazonaws.com", managed_policies=["arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"]),
+        FargatePodRole=Role("fargate-pod-role", "eks-fargate-pods.amazonaws.com", managed_policies=["arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"]),
+        NodegroupRole=Role("nodegroup-role", "ec2.amazonaws.com", managed_policies=[
+            "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+            "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+            "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+        ]),
+        ClusterVPC=VPC(name_prefix="cluster-vpc", num_public_subnet=2, num_private_subnet=2)
+    )
+
+    try:
+        resources.bootstrap()
+    except BootstrapFailureException as ex:
+        exit(254)
+
+    return resources
+
+if __name__ == "__main__":
+    config = service_bootstrap()
+    # Write config to current directory by default
+    config.serialize(bootstrap_directory)
