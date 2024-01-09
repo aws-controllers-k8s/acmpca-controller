264df06e486bfd98d0f835082f91fb27b91013de
Neha Bhat <ndbhat@amazon.com>
2024-01-09 12:08:20 -0500

    Add a method GetClient() that returns a k8s dynamic client

---
+++ pkg/client/client.go
@@ -0,0 +1,38 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package client
+
+import (
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+)
+
+var (
+	dynClient *dynamic.DynamicClient
+)
+
+func GetClient() (client *dynamic.DynamicClient, err error) {
+	if dynClient == nil {
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		dynClient, err = dynamic.NewForConfig(config)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return dynClient, nil
+}
