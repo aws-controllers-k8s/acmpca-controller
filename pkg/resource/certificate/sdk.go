08edc7357c157c6c2de3d15c50e0da056ab7ac62
Neha Bhat <ndbhat@amazon.com>
2024-01-18 13:03:18 -0500

    Set all Certificate resource spec fields to immutable

--- pkg/resource/certificate/sdk.go
+++ pkg/resource/certificate/sdk.go
@@ -617,3 +617,33 @@ func (rm *resourceManager) terminalAWSError(err error) bool {
 		return false
 	}
 }
+
+// getImmutableFieldChanges returns list of immutable fields from the
+func (rm *resourceManager) getImmutableFieldChanges(
+	delta *ackcompare.Delta,
+) []string {
+	var fields []string
+	if delta.DifferentAt("Spec.APIPassthrough") {
+		fields = append(fields, "APIPassthrough")
+	}
+	if delta.DifferentAt("Spec.CSR") {
+		fields = append(fields, "CSR")
+	}
+	if delta.DifferentAt("Spec.CertificateAuthorityARN") {
+		fields = append(fields, "CertificateAuthorityARN")
+	}
+	if delta.DifferentAt("Spec.SigningAlgorithm") {
+		fields = append(fields, "SigningAlgorithm")
+	}
+	if delta.DifferentAt("Spec.TemplateARN") {
+		fields = append(fields, "TemplateARN")
+	}
+	if delta.DifferentAt("Spec.Validity") {
+		fields = append(fields, "Validity")
+	}
+	if delta.DifferentAt("Spec.ValidityNotBefore") {
+		fields = append(fields, "ValidityNotBefore")
+	}
+
+	return fields
+}
